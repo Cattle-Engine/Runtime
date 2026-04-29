beda9574b12533bf46e345c93f896f0d08689eb4
tbaggeroftheuk <theodenabarker@gmail.com>
2026-04-29 08:50:42 +0100

    Fixed CmakeLists and removed workflow

--- source/engine/common/error_box.cpp
+++ source/engine/common/error_box.cpp
@@ -4,24 +4,13 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic pop
 
-extern "C" {
-#include "third_party/tiny_file_dialogs/tinyfiledialogs.h"
-}
+#include <SDL3/SDL.h>
 
 void ShowError(const std::string& msg) {
-    std::string safe_msg;
-    safe_msg.reserve(msg.size());
-    for (char c : msg) {
-        if (c == '"' || c == '\'') safe_msg += '`'; // replace both single and double quotes
-        else if (c == '\n' || c == '\r') safe_msg += ' '; // replace newlines
-        else safe_msg += c;
-    }
-
-    tinyfd_messageBox(
-        "Fatal error was caught!",
-        safe_msg.c_str(),
-        "ok",
-        "error",
-        1
+    SDL_ShowSimpleMessageBox(
+        SDL_MESSAGEBOX_ERROR,
+        "Fatal Error!",
+        msg.c_str(),
+        nullptr
     );
 }
