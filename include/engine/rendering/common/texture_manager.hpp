61d8c9d4cd077a36c3dd8d69785a24c4766b4d46
tbaggeroftheuk <211108056+tbaggeroftheuk@users.noreply.github.com>
2026-06-04 20:50:08 +0100

    New texture manager

---
+++ include/engine/rendering/common/texture_manager.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "engine/common/fs/vfs.hpp"
+#include "engine/rendering/renderer.hpp"
+
+namespace CE::Renderer::Resources {
+    using TextureHandle = uint64_t;
+    
+    // Inlined because I don't really want to pollute texture_manager.cpp
+    class TextureRef {
+        public:
+            TextureRef() = default;
+
+            TextureRef(TextureManager* mgr, TextureHandle handle, Texture* tex)
+                : mManager(mgr), mHandle(handle), mTexture(tex) {}
+
+            ~TextureRef() {
+                if (mManager)
+                    mManager->Return(mHandle);
+            }
+
+            Texture* Get() const { return mTexture; }
+
+            TextureRef(const TextureRef&) = delete;
+            TextureRef& operator=(const TextureRef&) = delete;
+
+            TextureRef(TextureRef&& other) noexcept {
+                *this = std::move(other);
+            }
+
+            TextureRef& operator=(TextureRef&& other) noexcept {
+                if (this != &other)
+                {
+                    mManager = other.mManager;
+                    mHandle = other.mHandle;
+                    mTexture = other.mTexture;
+
+                    other.mManager = nullptr;
+                    other.mTexture = nullptr;
+                }
+                return *this;
+            }
+
+    private:
+        TextureManager* mManager = nullptr;
+        TextureHandle mHandle = 0;
+        Texture* mTexture = nullptr;
+    };    
+    
+    class TextureManager {
+        public:
+            TextureManager(VFS::VFS& vfs, IRenderer& renderer);
+            
+            // Internal use for other systems only!
+            TextureRef Acquire(TextureHandle handle);
+            
+            // Used to decrease RefCount in TextureEntry
+            void Return(TextureHandle handle);
+            
+            // Returns InvalidTextureHandle if failure
+            TextureHandle Load(std::string path);
+            
+            // When RefCount in TextureEntry a texture is fully deleted
+            void Unload(TextureHandle handle);
+
+            // Called at end of the frame
+            void UnloadPendingDeletions();
+        private:
+            struct TextureEntry {
+                Texture* Resource;
+                uint32_t RefCount;
+                std::string Path;
+                bool IsError;
+                bool IsPendingUnload;
+            };
+
+            // int because so I can check if it's valid
+            TextureEntry* GetTextureEntry(TextureHandle handle);
+
+            IRenderer& mRenderer;
+            VFS::VFS& mVFS;
+            uint64_t mNextHandleID = 0;
+            std::vector<TextureEntry> mPendingDeletions;
+            std::unordered_map<TextureHandle, TextureEntry> mTextureCache;
+    };
+}
---
+++ source/engine/rendering/common/texture_manager.cpp
@@ -0,0 +1,89 @@
+#include "engine/rendering/common/texture_manager.hpp"
+#include "engine/common/tracelog.hpp"
+
+namespace CE::Renderer::Resources {
+    TextureManager::TextureManager(VFS::VFS& vfs, IRenderer& renderer) : mVFS(vfs), mRenderer(renderer) {}
+
+    TextureManager::TextureEntry* TextureManager::GetTextureEntry(TextureHandle handle) {
+        auto list = mTextureCache.find(handle);
+        if (list != mTextureCache.end()) {
+            return &list->second;
+        } 
+        return nullptr;
+    }
+
+    TextureRef TextureManager::Acquire(TextureHandle handle) {
+        TextureEntry* entry = GetTextureEntry(handle);
+        if (!entry || entry->IsPendingUnload)
+            return {};
+
+        entry->RefCount++;
+        return TextureRef(this, handle, entry->Resource);
+    }
+
+    void TextureManager::Return(TextureHandle handle) {
+        TextureEntry* entry = GetTextureEntry(handle);
+        if (entry) {
+            if (entry->RefCount > 0) entry->RefCount--;
+            return;
+        }
+    }
+
+    TextureHandle TextureManager::Load(std::string path) {
+        TextureEntry texentry = {};
+
+        if (!mVFS.FileExists(path.c_str())) {
+            CE::Log(LogLevel::Error, "[Texture Manager] File doesn't exist: {}", path);
+            texentry.Resource = mRenderer.GetErrorTexture();
+            texentry.IsError = true;
+        } else {
+            Texture* tex = mRenderer.LoadTex(path.c_str());
+
+            if (tex == nullptr) {
+                CE::Log(LogLevel::Error, "[Texture Manager] Failed to load texture: {}", path);
+                texentry.Resource = mRenderer.GetErrorTexture();
+                texentry.IsError = true;
+            } else {
+                texentry.Resource = tex;
+                texentry.IsError = false;
+            }
+        }
+
+        TextureHandle handle = mNextHandleID++;
+        texentry.Path = path;
+        texentry.RefCount = 0;
+        mTextureCache.emplace(handle, std::move(texentry));
+        return handle;
+    }
+
+    void TextureManager::Unload(TextureHandle handle) {
+        auto it = mTextureCache.find(handle);
+        if (it == mTextureCache.end()) {
+            CE::Log(LogLevel::Error, "[Texture Manager] Tried to unload an already unloaded texture: {}", handle);
+            return;
+        }
+        it->second.IsPendingUnload = true;
+    }
+
+    void TextureManager::UnloadPendingDeletions() {
+        for (auto it = mTextureCache.begin(); it != mTextureCache.end(); ) {
+            TextureEntry& entry = it->second;
+
+            if (entry.IsError) {
+                it = mTextureCache.erase(it);
+                continue;
+            }
+
+            if (entry.IsPendingUnload && entry.RefCount == 0) {
+                if (entry.Resource) {
+                    mRenderer.UnloadTex(entry.Resource);
+                    entry.Resource = nullptr;
+                }
+
+                it = mTextureCache.erase(it);
+            } else {
+                ++it;
+            }
+        }
+    }
+}
