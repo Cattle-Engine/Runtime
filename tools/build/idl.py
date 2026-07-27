d0f297e55e2c228f7c31a15710a37e207c3f1424
tbaggeroftheuk <211108056+tbaggeroftheuk@users.noreply.github.com>
2026-07-27 21:27:23 +0100

    Created IDL example

--- tools/build/idl.py
+++ tools/build/idl.py
@@ -1,4 +1,18 @@
 # Takes a yaml file and generates a C++ binding for angelscript
 
+import argparse
+from pathlib import Path
+
 def generate_comment(comment: str) -> str:
     return f"/* {comment} */"
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser("Build helper for CE to turn a yaml registration file into AngelScript bindings")
+    parser.add_argument("--source_dir", type=Path, help="Directory for source")
+    parser.add_argument("--include_dir", type=Path, help="Directory for include")
+    parser.add_argument("--yaml_file", type=Path, help="Path to the yaml file")
+    parser.add_argument("--output_dir", type=Path, help="Output directory for the generated header and source")
+    return parser.parse_args()
+
+def main() -> int:
+    args = parse_args()
