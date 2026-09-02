be23f7302aa82ea26188bda2c250208f1871a87e
tbaggeroftheuk <211108056+tbaggeroftheuk@users.noreply.github.com>
2026-09-02 21:33:05 +0100

    been thinking about how toe xpose ASCubemap

--- include/engine/scripting/bindings/as_cubemap.hpp
+++ include/engine/scripting/bindings/as_cubemap.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
 #include "engine/rendering/renderer.hpp"
+#include "engine/rendering/resources/texture_manager.hpp"
 
 namespace CE::Scripting::Bindings {
+    using TexRef = CE::Renderer::Resources::TextureRef;
+    using TexHandle = CE::Renderer::Resources::TextureHandle;
+    
     struct ASCubemap {
         // not exposed to AS
         CE::Renderer::CubeMap cubemap;
+
+        // also not exposed to as
+        TexRef face_right_ref;
+        TexRef face_left_ref;
+        TexRef face_top_ref;
+        TexRef face_bottom_ref;
+        TexRef face_front_ref;
+        TexRef face_back_ref;
+
+        TexHandle face_right_handle;
     };
 }
